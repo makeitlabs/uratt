19dd8a8d3e736e45bcff4cd6e321980dc0ebaaf5
Steve Richardson <tangentaudio@gmail.com>
2021-11-24 09:12:17 -0500

    create CMakeLists for project, change name of embedded binary certs (eventually move away from these)

--- firmware/esp32/main/net_mqtt.c
+++ firmware/esp32/main/net_mqtt.c
@@ -74,10 +74,10 @@
 
 static const char *TAG = "net_mqtt";
 
-extern const uint8_t client_cert_pem_start[] asm("_binary_client_uratt_crt_start");
-extern const uint8_t client_cert_pem_end[] asm("_binary_client_uratt_crt_end");
-extern const uint8_t client_key_pem_start[] asm("_binary_client_uratt_key_start");
-extern const uint8_t client_key_pem_end[] asm("_binary_client_uratt_key_end");
+extern const uint8_t client_cert_pem_start[] asm("_binary_client_crt_start");
+extern const uint8_t client_cert_pem_end[] asm("_binary_client_crt_end");
+extern const uint8_t client_key_pem_start[] asm("_binary_client_key_start");
+extern const uint8_t client_key_pem_end[] asm("_binary_client_key_end");
 extern const uint8_t ca_cert_pem_start[] asm("_binary_ca_crt_start");
 extern const uint8_t ca_cert_pem_end[] asm("_binary_ca_crt_end");
 
