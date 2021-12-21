0ed893c0eac0dd9be9cc4a2ab7e296351b38adb8
Steve Richardson <tangentaudio@gmail.com>
2021-12-20 23:23:43 -0500

    fix access screen backgrounds (no canvas)

--- firmware/main/ui/ui_access.c
+++ firmware/main/ui/ui_access.c
@@ -2,10 +2,8 @@
 #include "lvgl.h"
 #include "display_task.h"
 
-typedef struct {
-    lv_obj_t *scr;
-    int count_val;
-} my_timer_context_t;
+
+static lv_style_t style;
 
 static lv_obj_t *label_user = NULL;
 
@@ -13,9 +11,10 @@ static lv_obj_t *label_user = NULL;
 void ui_access_set_user(char *user, bool allowed)
 {
   lv_color_t color = allowed ? lv_palette_main(LV_PALETTE_GREEN) : lv_palette_main(LV_PALETTE_RED);
+  lv_style_set_bg_color(&style, color);
+  lv_obj_add_style(label_user->parent, &style, 0);
 
-  lv_label_set_text(label_user, user);
-  // TODO: change bg color
+  lv_label_set_text_fmt(label_user, "%s %s", allowed ? LV_SYMBOL_OK : LV_SYMBOL_CLOSE, user);
 }
 
 
@@ -23,12 +22,18 @@ lv_obj_t* ui_access_create()
 {
   lv_obj_t* scr = lv_obj_create(NULL);
 
+  lv_style_init(&style);
+  lv_style_set_radius(&style, 0);
+  lv_style_set_bg_color(&style, lv_color_black());
+
+  lv_obj_add_style(scr, &style, 0);
+
   label_user = lv_label_create(scr);
   lv_label_set_long_mode(label_user, LV_LABEL_LONG_SCROLL_CIRCULAR);
   lv_obj_set_width(label_user, 156);
   lv_obj_align(label_user, LV_ALIGN_CENTER, 0, 0);
   lv_obj_set_style_text_font(label_user, &lv_font_montserrat_36, 0);
-//  lv_obj_set_style_text_color(label_user, lv_color_white(), 0);
+  lv_obj_set_style_text_color(label_user, lv_color_white(), 0);
 
   return scr;
 }
